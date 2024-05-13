51283c43076e92520e938e6dd1dacfc895e52cde
Pupaeeiei <158018566+Pupaeeiei@users.noreply.github.com>
2024-05-13 16:32:41 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,157 @@
+tourpoint =
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao Yai National Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.49691846933501,
+            14.3308133656749
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Grand Palace",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49169443345437,
+            13.75101629854953
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Iconsiam",
+          "type": "natural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.51043650000179,
+            13.726709415708115
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Chiang Khan",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            101.70128399999987,
+            17.90692454016468
+          ],
+          "type": "Point"
+        },
+        "id": 3
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Doi Inthanon",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            98.48746567064114,
+            18.58847727029655
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Tha Phae Gate",
+          "type": "cutural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            98.9935379999983,
+            18.78781081147291
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Haew Narok Waterfall",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            101.39352499999995,
+            14.287271523791304
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Mon Cham",
+          "type": "natural ",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            98.82269599999859,
+            18.935450525812016
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Chom Daew Rafting",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            99.8778530846559,
+            7.891565129209738
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khi ri wong Waterfall",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            99.77396544150196,
+            8.436450881456537
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
